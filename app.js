dde864d743dee5c669a7afc1023e4a525a43412f
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-01-11 14:13:01 +0100

    ajout produit et voir panier

--- app.js
+++ app.js
@@ -1,66 +1,125 @@
 // Variables
-let modal = document.querySelector(".modal");
-let modalTitle = document.querySelector(".modal-title");
-let panier = document.getElementById("cart");
-let modalBody = document.querySelector(".modal-body");
-//
-// Fonction AJOUT dans le Panier
+// Fonction Voir produit et AJOUT dans le Panier
 let container = document.querySelector(".pro");
+let text = document.querySelector(".text");
+let header = document.querySelector(".bg");
+let panier = document.querySelector(".panier");
 const tabProduct = [
   {
     id: 1,
     product: "roberouge.jpg",
+    title: "Robe de soirée",
+    price: 124.99,
   },
   {
     id: 2,
     product: "redDress.jpg",
-    title: "Pull-over torsadé foil",
-    price: 59.99,
+    title: "Robe de soirée",
+    price: 299.99,
   },
   {
     id: 3,
     product: "yellowDress.jpg",
-    title: "Cardigan liserés contrastants",
-    price: 79.99,
+    title: "Robe de soirée",
+    price: 179.99,
   },
   {
     id: 4,
     product: "soie.jpg",
-    title: "Pull-over oversize col V",
-    price: 69.99,
+    title: "Robe de soirée",
+    price: 169.99,
   },
   {
     id: 5,
     product: "dress.jpg",
-    title: "test",
-    price: 69.99,
+    title: "Robe de soirée",
+    price: 180.99,
   },
   {
     id: 6,
     product: "robemarron.jpg",
-    title: "test",
-    price: 69.99,
+    title: "Robe de soirée",
+    price: 110.99,
   },
 ];
-for (i = 0; i < tabProduct.length; i++) {
-  container.innerHTML += `<div class="">
-    <div class="">
-        <div class="card border-0 ">
-            <img style="height: 480px;" src="images/${tabProduct[i].product}" id="tt" class="shadow" alt="">
-            <div class="card-body">
-                <h4 class="clothes text-white fw-bold"></h4>
-                <div class="d-flex mt-2 justify-content-center align-items-center">
-                  <button class="w-100  p-1 ajout">Voir produit</button>
+
+container.innerHTML = "";
+let getProduct = tabProduct.map(
+  (p) =>
+    (container.innerHTML += `<div class="mt-4">
+              <div class="">
+                <div class="card border-0 ms-3 carte ">
+                    <img style="height: 480px;" src="images/${p.product}" id="tt" class="shadow" alt="">
+                    <div class="card-body">
+                        <h4 class="clothes text-white fw-bold"></h4>
+                        <div class="test d-flex mt-2 justify-content-center align-items-center">
+                         <button id="${p.id}">Voir le produit</button>
+                        </div>
+                    </div>
                 </div>
             </div>
+          </div>`)
+);
+let urlComplet = window.location.pathname;
+let buttons = document.querySelectorAll(".card button");
+let cards = document.querySelectorAll(".carte");
+console.log(cards);
+for (i = 0; i < tabProduct.length; i++) {
+  for (i = 0; i < buttons.length; i++) {
+    let test = tabProduct[i];
+    let btn = buttons[i];
+    buttons[i].addEventListener("click", function () {
+      container.style.backgroundColor = "#303952";
+      container.style.padding = "40px";
+      // urlComplet = "/ajout.html";
+      console.log(test);
+      header.innerHTML = "";
+      text.innerHTML = "";
+      container.innerHTML = `<div class="row">
+      <div class="col-lg-6">
+      <img src="images/${test.product}" class="me-5 w-100">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="text-warning">${test.title}</h1>
+        <p>Sold by: Fabi's shop</p>
+        <p>
+        <i class="fa fa-star text-warning" aria-hidden="true"></i>
+        <i class="fa fa-star  text-warning" aria-hidden="true"></i>
+        <i class="fa fa-star  text-warning" aria-hidden="true"></i>
+        <i class="fa fa-star-half-o  text-warning" aria-hidden="true"></i>
+        <i class="fa fa-star-o text-secondary" aria-hidden="true">(200)</i> 
+        </p>
+        
+      <div class="row d-flex align-items-center">
+          <div class="col">
+          <label class="text-secondary">Size: </label>
+          </div>
+          <div class="col">
+          <select name="" id="" class="p-1 border border-secondary rounded" style="width: 120px;">
+            <option value="">Select size</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+        </select>
         </div>
-    </div>
-  </div>`;
-  let button = document.querySelectorAll(".ajout");
-  console.log(button);
-  for (i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", function () {
-      alert("hello");
+      </div>
+
+     
+      <button class="w-100 mt-5 border-0 btnCart">Add to cart</button>
+      </div>
+     
+      </div>
+      </div>`;
+
+      let btnAdd = document.querySelector(".btnCart");
+      let n = 1;
+
+      //
+      btnAdd.addEventListener("click", function () {
+        panier.innerHTML = n++;
+      });
     });
   }
 }
+
+// console.log(urlComplet);
