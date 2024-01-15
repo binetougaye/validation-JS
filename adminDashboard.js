406ce323c4bd651a46a63f553326cd1b620e9047
ZooM98 <haurlyroll@gmail.com>
2024-01-15 12:01:02 +0000

    Ajout de la fonction de redirection admin/client

---
+++ adminDashboard.js
@@ -0,0 +1,130 @@
+// Firestore
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+let user = 0;
+let tbody = document.getElementById('tbody');
+
+function addItemToTable(prenom, nom, email, action) {
+let trow = document.createElement('tr');
+let td1 = document.createElement('td');
+let td2 = document.createElement('td');
+let td3 = document.createElement('td');
+let td4 = document.createElement('td');
+let td5 = document.createElement('td');
+
+td1.innerHTML = ++user;
+td2.innerHTML = prenom;
+td3.innerHTML = nom;
+td4.innerHTML = email;
+td5.innerHTML = action;
+
+trow.appendChild(td1);
+trow.appendChild(td2);
+trow.appendChild(td3);
+trow.appendChild(td4);
+trow.appendChild(td5);
+
+
+td5.innerHTML = `<i class="bi bi-eye"></i> <i class="bi bi-trash3" id="test" "></i>`
+
+tbody.appendChild(trow);
+
+// Ajoute un écouteur d'événements sur le tableau lui-même
+tbody.addEventListener('click', function (event) {
+    // Vérifie si l'élément cliqué est une icône de corbeille
+    if (event.target.classList.contains('bi-trash3')) {
+        // Supprime la ligne parente de l'icône de corbeille
+        let rowToRemove = event.target.closest('tr');
+        rowToRemove.remove();
+    }
+});
+
+}
+
+
+function addAllItemToTable(users) {
+    user = 0;
+    tbody.innerHTML = '';
+    users.forEach(element => {
+        addItemToTable(element.firstname, element.lastname, element.email, element.action);
+    });
+}
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWpBSXZ0zYov-GkuQRl5vfiq9V6UkfsDE",
+    authDomain: "validation-js-2024.firebaseapp.com",
+    databaseURL: "https://validation-js-2024-default-rtdb.firebaseio.com",
+    projectId: "validation-js-2024",
+    storageBucket: "validation-js-2024.appspot.com",
+    messagingSenderId: "565787116440",
+    appId: "1:565787116440:web:d6eb3365dbd8c71d70a1ed",
+    measurementId: "G-E3S1WFF6H2"
+};
+
+
+// Initialize Firestore
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+
+// Firestore
+
+function getAllDataOnce() {
+    const userAutListRef = collection(db, 'UserAutList');
+    getDocs(userAutListRef)
+        .then((querySnapshot) => {
+            let utilisateurs = [];
+
+            querySnapshot.forEach((doc) => {
+                utilisateurs.push(doc.data());
+            });
+
+            addAllItemToTable(utilisateurs);
+            
+            // recuperation nombre d'utilisateurs
+            document.getElementById('client').innerHTML = utilisateurs.length
+            document.getElementById('clients').innerHTML = utilisateurs.length
+            })
+        .catch((error) => {
+            console.error("Error getting documents: ", error);
+        });
+}
+
+// Recuperation des données en temps reel
+// function getAllDataRealTime() {
+//     const dbRef = ref(db, 'UserAutList');
+
+//     onValue(dbRef, (snapshot) => {
+//         let utilisateurs = [];
+//         snapshot.forEach(childSnapshot => {
+//             utilisateurs.push(childSnapshot.val())
+//         });
+//         addAllItemToTable(utilisateurs);
+//     })
+// }
+
+                    
+
+window.onload = getAllDataOnce;
+
+                    // Regle firestore
+                    // allow read, write: if request.auth != null;
+
+
+
+
+                    // // Suppression d'un utilisateur
+                    // let supp = document.getElementById('test')
+
+                    // supp.addEventListener('click', function () {
+                    
+                    //     let toDel = trow.innerHTML;
+                    //     toDel.innerHTML = '';
+                    // })
+
